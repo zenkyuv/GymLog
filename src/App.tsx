fd4ecde6bafe3288b74cf4373c3468fd212d5a4f
Przemek Gałęzki <przemekgg2002@gmail.com>
2022-01-16 23:35:21 +0100

    add: observer for component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
-import React from 'react';
-import Mainpage from './html-components/mainpage/main-page';
-import './App.css';
+import React, { useContext } from 'react';
+import Dashboard from './html-components/dashboard/dashboard';
+import { observer } from 'mobx-react-lite';
+import PageStore from './html-components/states/page-store';
+import MainPage from './html-components/mainpage';
 
-function App() {
+const App = observer(() => {
+	const pageStore = useContext(PageStore)
 	return (
 		<>
-			<Mainpage/>
-			</>
+			{pageStore.dashboardVisible === false ? <MainPage /> : <Dashboard />}
+		</>
   );
-}
+})
 
 export default App;
