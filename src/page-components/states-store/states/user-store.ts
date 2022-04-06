a72d63a2240b0609498316dedbd4db6ff2bf5b0b
Przemek Gałęzki <przemekgg2002@gmail.com>
2022-04-06 13:13:34 +0200

    add: new observables for workout data from database

--- src/page-components/states-store/states/user-store.ts
+++ src/page-components/states-store/states/user-store.ts
@@ -1,31 +1,56 @@
-import { action, makeObservable } from "mobx";
-import { observable } from "mobx"
-import { createContext } from "react";
-import { useState } from "react";
+import { action, makeObservable } from 'mobx';
+import { observable } from 'mobx';
+import { createContext } from 'react';
+import { useState } from 'react';
 
 export class UserStore {
-	@observable
-	userLogged = false;
-	userUID = ""
+  @observable userLogged = false;
+  userUID = '';
+  @observable workoutData: any = [];
+  choosenExercise: any = [];
+  @observable dbDataLoading: any = true;
 
-	constructor() {
-	makeObservable(this)
+  constructor() {
+    makeObservable(this);
+  }
+
+  @action
+  NotLogged() {
+    this.userLogged = false;
+  }
+
+  @action
+  Logged() {
+    this.userLogged = true;
+  }
+
+  @action
+  setUserUID(uid: any) {
+    this.userUID = uid;
+  }
+
+  @action
+  setWorkoutData(data: any) {
+    this.workoutData = data;
+  }
+
+  @action
+  isDbDataLoading(boolean: boolean) {
+    if (boolean) {
+      this.dbDataLoading = true;
+    } else {
+      this.dbDataLoading = false;
+    }
+  }
+
+  @action
+  setChoosenExercises = (exercise: any, category: any) => {
+    this.choosenExercise.push({
+      exercise: exercise,
+      category: category,
+      isSet: exercise && category ? true : false,
+    });
+  };
 }
 
-	@action
-	NotLogged() {
-		this.userLogged = false;
-	}
-
-	@action
-	Logged() {
-		this.userLogged = true;
-	}
-
-	@action
-	setUserUID(uid:any) {
-		this.userUID = uid
-	}
-}
-
-export default createContext(new UserStore())
+export default createContext(new UserStore());
