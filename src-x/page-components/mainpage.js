a59dfd3722da6106796e47205a9c4e0083ffbd8b
Przemek Gałęzki <przemekgg2002@gmail.com>
2022-04-06 13:37:13 +0200

    remove: files being builded

--- src-x/page-components/mainpage.js
+++
@@ -1,9 +0,0 @@
-import { jsx as _jsx, jsxs as _jsxs } from "react/jsx-runtime";
-import { observer } from "mobx-react-lite";
-import Footer from "./common/footer";
-import Header from "./common/header";
-import Main from "./common/main";
-const MainPage = observer(() => {
-    return (_jsxs("div", { children: [_jsx(Header, {}, void 0), _jsx(Main, {}, void 0), _jsx(Footer, {}, void 0)] }, void 0));
-});
-export default MainPage;
