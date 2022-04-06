8b9d723e1729d3a14fe66f8611ef970a0c1c009c
Przemek Gałęzki <przemekgg2002@gmail.com>
2022-04-06 13:55:54 +0200

    refactor: make css easier to read(prettier)

--- src/page-components/common/dashboard/calendar/calendar-buttons.tsx
+++ src/page-components/common/dashboard/calendar/calendar-buttons.tsx
@@ -1,66 +1,123 @@
-import { daysOfCurrentMonths } from "./helpers";
+import { daysOfCurrentMonths } from './helpers';
 
-
-export function handleMonthNavBackButtonClick({ component, yearAndMonth, onYearAndMonthChange }: {
-	component: string
-	yearAndMonth: any
-	onYearAndMonthChange: any
+export function handleMonthNavBackButtonClick({
+  setExercisesPanel,
+  component,
+  yearAndMonth,
+  onYearAndMonthChange,
+  userStore,
+}: {
+  setExercisesPanel?: any;
+  component: string;
+  yearAndMonth: any;
+  onYearAndMonthChange: any;
+  userStore: any;
 }) {
-	console.log(yearAndMonth)
-	const [year, month, day] = yearAndMonth;
-	let prevDay = day - 1
-	let nextYear = year;
-	let nextMonth = month;
-	if (component === "workoutPanel") {
-		if (prevDay < 1) {
-			nextMonth -= 1
-			prevDay = daysOfCurrentMonths
-		}
-	}
-		else if (component === 'calendar') {
-		nextMonth -= 1;
-	}
-			if (nextMonth === 0) {
-			nextMonth = 12;
-			nextYear = year - 1;
-		}
-		onYearAndMonthChange([nextYear, nextMonth, prevDay]);
+  console.log(yearAndMonth);
+  const [year, month, day] = yearAndMonth;
+  let prevDay = day - 1;
+  userStore.workoutData = [];
+  let nextYear = year;
+  let nextMonth = month;
+  if (component === 'workoutPanel') {
+    setExercisesPanel({
+      showExercise: false,
+      category: undefined,
+      exercise: undefined,
+      controlPanel: false,
+    });
+    if (prevDay < 1) {
+      nextMonth -= 1;
+      prevDay = daysOfCurrentMonths;
+    }
+  } else if (component === 'calendar') {
+    nextMonth -= 1;
+  }
+  if (nextMonth === 0) {
+    nextMonth = 12;
+    nextYear = year - 1;
+  }
+  onYearAndMonthChange([nextYear, nextMonth, prevDay]);
 }
 
-export function handleMonthNavForwardButtonClick({component, yearAndMonth, onYearAndMonthChange }:
-	{	
-		component:any
-		yearAndMonth: any
-		onYearAndMonthChange: any
-	}) {
-	const [year, month, day] = yearAndMonth;
-    let nextYear = year;
-	let nextMonth = month;
-	let nextDay = day + 1;
-	if(component === "workoutPanel") {
-	if (nextDay > daysOfCurrentMonths) {
-		nextDay = 1
-		nextMonth += 1;
-		}
-	}
-	else if (component === 'calendar') {
-		nextMonth += 1;
-	}
-    if (nextMonth === 13) {
-      nextMonth = 1;
-      nextYear = year + 1;
+export function handleMonthNavForwardButtonClick({
+  setExercisesPanel,
+  component,
+  yearAndMonth,
+  onYearAndMonthChange,
+  userStore,
+}: {
+  setExercisesPanel?: any;
+  component: any;
+  yearAndMonth: any;
+  onYearAndMonthChange: any;
+  userStore: any;
+}) {
+  const [year, month, day] = yearAndMonth;
+  let nextYear = year;
+  let nextMonth = month;
+  let nextDay = day + 1;
+  userStore.workoutData = [];
+  if (component === 'workoutPanel') {
+    setExercisesPanel({
+      showExercise: false,
+      category: undefined,
+      exercise: undefined,
+      controlPanel: false,
+    });
+    if (nextDay > daysOfCurrentMonths) {
+      nextDay = 1;
+      nextMonth += 1;
     }
-    onYearAndMonthChange([nextYear, nextMonth, nextDay]);
-};
-	
+  } else if (component === 'calendar') {
+    nextMonth += 1;
+  }
+  if (nextMonth === 13) {
+    nextMonth = 1;
+    nextYear = year + 1;
+  }
+  onYearAndMonthChange([nextYear, nextMonth, nextDay]);
+}
 
-export function renderButtons(componentBetween:any, component:any, yearAndMonth:any, onYearAndMonthChange:any) {
-	return (
-		<div className="time-cnt">
-			<button onClick={() => handleMonthNavBackButtonClick({ component, yearAndMonth, onYearAndMonthChange })}>{"<"}</button>
-				{componentBetween}
-			<button onClick={() => handleMonthNavForwardButtonClick({component,yearAndMonth, onYearAndMonthChange})}>{">"}</button>
-		</div>	
-	)
-
+export function renderButtons(
+  setExercisesPanel: any,
+  componentBetween: any,
+  component: any,
+  yearAndMonth: any,
+  onYearAndMonthChange: any,
+  userStore: any
+) {
+  return (
+    <div className="time-cnt">
+      <button
+        className="button-margin"
+        onClick={() =>
+          handleMonthNavBackButtonClick({
+            setExercisesPanel,
+            component,
+            yearAndMonth,
+            onYearAndMonthChange,
+            userStore,
+          })
+        }
+      >
+        {'<'}
+      </button>
+      {componentBetween}
+      <button
+        className="button-margin"
+        onClick={() =>
+          handleMonthNavForwardButtonClick({
+            setExercisesPanel,
+            component,
+            yearAndMonth,
+            onYearAndMonthChange,
+            userStore,
+          })
+        }
+      >
+        {'>'}
+      </button>
+    </div>
+  );
 }
