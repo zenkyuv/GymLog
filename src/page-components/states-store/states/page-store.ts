158948e54a2e98d26242d9f08f4fe8ee55d8a4a0
Przemek Gałęzki <przemekgg2002@gmail.com>
2022-04-06 13:19:46 +0200

    refactor: code style using prettier

--- src/page-components/states-store/states/page-store.ts
+++ src/page-components/states-store/states/page-store.ts
@@ -1,26 +1,22 @@
-import { action, makeObservable } from "mobx";
-import { observable } from "mobx"
-import { createContext } from "react";
+import { action, makeObservable } from 'mobx';
+import { observable } from 'mobx';
+import { createContext } from 'react';
 
 export class PageStore {
-	@observable dashboardVisible = false
+  @observable dashboardVisible = false;
 
-	constructor() {
-	makeObservable(this)
+  constructor() {
+    makeObservable(this);
+  }
+
+  @action makeDashboardVisible = () => {
+    this.dashboardVisible = true;
+  };
+
+  @action makeDashboardNotVisible = () => {
+    this.dashboardVisible = false;
+  };
 }
+const pageStore = new PageStore();
 
-	@action makeDashboardVisible = () => {
-		this.dashboardVisible = true;
-	}
-
-	@action makeDashboardNotVisible = () => {
-		this.dashboardVisible = false;
-	}
-
-	
-	
-}
-const pageStore = new PageStore()
-
-export default createContext(pageStore)
-
+export default createContext(pageStore);
