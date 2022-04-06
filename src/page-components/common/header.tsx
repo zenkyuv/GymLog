e4801b62debb265d584249258f112d8c434fbadc
Przemek Gałęzki <przemekgg2002@gmail.com>
2022-04-06 13:59:03 +0200

    refactor: make css easier to read(prettier)

--- src/page-components/common/header.tsx
+++ src/page-components/common/header.tsx
@@ -1,45 +1,79 @@
-import React, { useContext, useEffect, useState } from "react";
-import "../../component-styles/header.css"
-import { logout } from "../auth";
-import Register from "../register-login-panel";
-import { observer } from "mobx-react-lite";
-import  UserStore  from "../states-store/states/user-store";
-import  PageStore  from "../states-store/states/page-store";
-const Header = observer(() => {
-	
-	const userStore = useContext(UserStore)
-	const pageStore = useContext(PageStore)
-	const [clicked, setClick] = useState(false)
+import React, { useContext, useEffect, useRef, useState } from 'react';
+import '../../component-styles/header.css';
+import { logout } from '../auth';
+import Register from '../register-login-panel';
+import { observer } from 'mobx-react-lite';
+import UserStore from '../states-store/states/user-store';
+import PageStore from '../states-store/states/page-store';
+import classNames from 'classnames';
 
-	useEffect(() => {
-		if (userStore.userLogged) {
-			setClick(false)
-		} 
-		else if (userStore.userLogged && pageStore.dashboardVisible === false) {
-			setClick(true)
-		}
-	}, [pageStore.dashboardVisible, userStore.userLogged])
+const Header = observer((css: any) => {
+  const userStore = useContext(UserStore);
+  const pageStore = useContext(PageStore);
+  const [clicked, setClick] = useState(false);
+  useEffect(() => {
+    if (userStore.userLogged) {
+      setClick(false);
+    } else if (userStore.userLogged && pageStore.dashboardVisible === false) {
+      setClick(true);
+    }
+  }, [pageStore.dashboardVisible, userStore.userLogged]);
 
-	return (
-		<header className="header-area header-sticky header-color">
-			<nav className="main-nav">
-				<a href="index.html" className="logo">Gym<em> Log</em><span className="line">-</span><span>simple workout tracker</span></a>
-				<ul className="nav">
-					{userStore.userLogged ? <li className="scroll-to-section"><a href="#top" className="active" onClick={() => pageStore.makeDashboardVisible()}>Dashboard</a></li> :
-					null}
-					{userStore.userLogged === false
-						? <li className="main-button"><a href="#/" onClick={() => setClick(true)}>Sign up</a></li>
-						: <li className="main-button"><a href="#/" onClick={() => logout(userStore, pageStore)}>Sign out</a></li>}
-				</ul>
-				<a className="menu-trigger" href="#/">
-					<span>Menu</span>
-				</a>
-			</nav>
-			{clicked === true && userStore.userLogged === false
-				? <Register />
-				: null}
-		</header>
-	)
-})
+  const setCss = Object.keys(css).length !== 0;
+  return (
+    <header
+      className={classNames(
+        'header-area header-sticky',
+        setCss ? 'dashboard-page-colors' : 'main-page-colors'
+      )}
+    >
+      <nav className="main-nav">
+        <a
+          href="index.html"
+          className={classNames(
+            'logo',
+            setCss ? 'dashboard-page-colors' : 'main-page-colors'
+          )}
+        >
+          Gym<span className="text">Log</span>
+          <span className="line">-</span>
+          <span className="info">simple workout tracker</span>
+        </a>
+        <div className="sign-buttons">
+          <ul className="nav">
+            {userStore.userLogged && !pageStore.dashboardVisible ? (
+              <li className="scroll-to-section">
+                <a
+                  href="#top"
+                  className="active"
+                  onClick={() => pageStore.makeDashboardVisible()}
+                >
+                  Dashboard
+                </a>
+              </li>
+            ) : null}
+            {userStore.userLogged === false ? (
+              <li className="main-button">
+                <a href="#/" onClick={() => setClick(true)}>
+                  Sign up
+                </a>
+              </li>
+            ) : (
+              <li className="main-button">
+                <a href="#/" onClick={() => logout(userStore, pageStore)}>
+                  Sign out
+                </a>
+              </li>
+            )}
+          </ul>
+          {/* <a className="menu-trigger" href="#/"> */}
+          <span className="bars">---</span>
+          {/* </a> */}
+        </div>
+      </nav>
+      {clicked === true && userStore.userLogged === false ? <Register /> : null}
+    </header>
+  );
+});
 
-export default Header
+export default Header;
--- src/page-components/register-login-panel.tsx
+++ src/page-components/register-login-panel.tsx
@@ -1,57 +1,97 @@
-import { observer } from "mobx-react-lite";
-import React, { useContext, useEffect, useState } from "react"
-import { signUser, createUser } from "./auth";
-import UserStore from "./states-store/states/user-store";
-import "../component-styles/register.css"
-import loadingIndicator from "../images/loading-indicator.svg"
+import { observer } from 'mobx-react-lite';
+import React, { useContext, useEffect, useState } from 'react';
+import { signUser, createUser } from './auth';
+import UserStore from './states-store/states/user-store';
+import '../component-styles/register.css';
+import loadingIndicator from '../images/loading-indicator.svg';
 
 const Register = observer(() => {
-	const userStore = useContext(UserStore)
-	const signupForm: any = React.useRef(null)
-	const [user, setUser] = useState({
-		email: "",
-		password: "",
-	})
+  const userStore = useContext(UserStore);
+  const signupForm: any = React.useRef(null);
+  const [user, setUser] = useState({
+    email: '',
+    password: '',
+  });
 
-	const [showLoadingIndicator, setLoadingIndicator] = useState(false)
+  const [showLoadingIndicator, setLoadingIndicator] = useState(false);
 
-	useEffect(() => {
-		const email = signupForm?.current['email']
-		const password = signupForm?.current['psw']
-		setUser({ email, password})
-	}, [])
+  useEffect(() => {
+    const email = signupForm?.current['email'];
+    const password = signupForm?.current['psw'];
+    setUser({ email, password });
+  }, []);
 
-	function spinner() {
-		if (userStore.userLogged === false) {
-			setLoadingIndicator(true)
-		}
-	}
+  function spinner() {
+    if (userStore.userLogged === false) {
+      setLoadingIndicator(true);
+    }
+  }
 
-const spinnerImage = <img className="spinner-img" src={loadingIndicator} alt=""/>
-	return (
-		<div className="form-popup" id="myForm">
-			<form action="/action_page.php" ref={signupForm} className="form-container" >
-		
-					<h1>Login</h1>
-		
-						<label htmlFor="email"><b>Email</b></label>
-						<input type="text" placeholder="Enter Email" name="email" id="email" required />
-		
-						<label htmlFor="psw"><b>Password</b></label>
-						<input type="password" placeholder="Enter Password" name="psw" id="psw" required />
-		
-						<button type="button" onClick={(e: any) =>
-						{ signUser(e, user.email, user.password, userStore, setLoadingIndicator); spinner() }}
-						className="btn">{showLoadingIndicator
-						? spinnerImage
-						: "Login"}
-						</button>
-						<button type="button" onClick={(e: any) => createUser(e, user.email, user.password, userStore)} className="btn">Register</button>
-						<button type="button" className="btn cancel" >Close</button>
-			</form>
-		</div>
-	)
-})
+  const spinnerImage = (
+    <img className="spinner-img" src={loadingIndicator} alt="" />
+  );
+  return (
+    <div className="form-popup" id="myForm">
+      <form
+        action="/action_page.php"
+        ref={signupForm}
+        className="form-container"
+      >
+        <h1>Login</h1>
 
-export default Register
+        <label htmlFor="email">
+          <b>Email</b>
+        </label>
+        <input
+          type="text"
+          placeholder="Enter Email"
+          name="email"
+          id="email"
+          required
+        />
 
+        <label htmlFor="psw">
+          <b>Password</b>
+        </label>
+        <input
+          type="password"
+          placeholder="Enter Password"
+          name="psw"
+          id="psw"
+          required
+        />
+
+        <button
+          type="button"
+          onClick={(e: any) => {
+            signUser(
+              e,
+              user.email,
+              user.password,
+              userStore,
+              setLoadingIndicator
+            );
+            spinner();
+          }}
+          className="btn"
+        >
+          {showLoadingIndicator ? spinnerImage : 'Login'}
+        </button>
+        <button
+          type="button"
+          onClick={(e: any) =>
+            createUser(e, user.email, user.password, userStore)
+          }
+          className="btn"
+        >
+          Register
+        </button>
+        <button type="button" className="btn cancel">
+          Close
+        </button>
+      </form>
+    </div>
+  );
+});
+
+export default Register;
