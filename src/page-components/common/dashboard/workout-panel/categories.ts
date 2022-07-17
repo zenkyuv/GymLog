d900f5342806506471b8937cb37234dc5d156e6e
Przemek Gałęzki <przemekgg2002@gmail.com>
2022-07-17 17:45:39 +0200

    add: list of workout categories and exercises

---
+++ src/page-components/common/dashboard/workout-panel/categories.ts
@@ -0,0 +1,141 @@
+export const categories = [
+	{
+		category: "Shoulders",
+		exercises: [
+				"Arnold Dumbell Press",
+				"Behind The Neck Barbell Press",
+				"Cable Face Pull",
+				"Front Dumbell Raise",
+				"Hammer Strength Shoulder Press",
+				"Lateral Dumbbell Raise",
+				"Lateral Machine Raise",
+				"Log Press",
+				"One-Arm Standing Dumbbell Press",
+				"Overhead Press",
+				"Push Press",
+				"Rear Delt Dumbbell Raise",
+				"Rear Delt Machine Fly",
+				"Seated Dumbbell Lateral Raise",
+				"Seated Dumbbell Press",
+				"Smith Machine Overhead Press"
+			]
+	},
+	{
+		category: "Legs",
+		exercises: [
+				"Barbell Calf Raise",
+				"Barbell Front Squat",
+				"Barbell Glute Bridge",
+				"Barbell Squat",
+				"Donkey Calf Raise",
+				"Glute-Ham Raise",
+				"Leg Extension Machine",
+				"Leg Press",
+				"Lying Leg Curl Machine",
+				"Romanian Deadlift",
+				"Seated Calf Raise Machine",
+				"Seated Leg Curl Machine",
+				"Standing Calf Raise Machine",
+				"Stiff-Legged Deadlift",
+				"Sumo Deadlift"
+			]
+	},
+	{
+		category: "Back",
+		exercises: [
+			"Barbell Row",
+			"Barbell Shrug",
+			"Chin Up",
+			"Conventional Deadlift",
+			"Deadlift",
+			"Dumbbell Row",
+			"Good Morning",
+			"Hammer Strength Row",
+			"Lat Pulldown",
+			"Machine Shrug",
+			"Neutral Chin Up",
+			"Pendlay Row",
+			"Pull Up",
+			"Rack Pull",
+			"Seated Cable Row",
+			"Straight-Arm Cable Pushdown",
+			"T-Bar Row"
+		]
+	},
+	{
+		category: "Biceps",
+		exercises: [
+			"Barbell Curl",
+			"Barbell Spider Curl",
+			"Cable Curl",
+			"Dumbbell Concentration Curl",
+			"Dumbbell Curl",
+			"Dumbbell Hammer Curl",
+			"Dumbbell Preacher Curl",
+			"EZ-Bar Curl",
+			"EZ-Bar Preacher Curl",
+			"Seated Incline Dumbbell Curl",
+			"Seated Machine Curl"
+		]
+	},
+	{
+		category: "Triceps",
+		exercises: [
+			"Cable Overhead Triceps Extension",
+			"Close Grip Barbell Bench Press",
+			"Dumbbell Overhead Triceps Extension",
+			"EZ-Bar Skullcrusher",
+			"Lying Triceps Extension",
+			"Parallel Bar Triceps Dip",
+			"Ring Dip",
+			"Rope Push Down",
+			"Smith Machine Close Grip Bench Press",
+			"V-Bar Push Down"
+		]
+	},
+	{
+		category: "Chest",
+		exercises: [
+			"Cable Crossover",
+			"Decline Barbell Bench Press",
+			"Decline Hammer Strength Chest Press",
+			"Flat Barbell Bench Press",
+			"Flat Barbell Bench Press With Pause",
+			"Flat Dumbbell Bench Press",
+			"Flat Dumbbell Fly",
+			"Incline Barbell Bench Press",
+			"Incline Dumbbell Bench Press",
+			"Incline Dumbbell Fly",
+			"Incline Hammer Strength Chest Press",
+			"Seated Machine Fly"
+		]
+	},
+	{
+		category: "Abs",
+		exercises: [
+			"Ab-Wheel Rollout",
+			"Cable Crunch",
+			"Crunch",
+			"Crunch Machine",
+			'Decline Crunch',
+			"Dragon Flag",
+			"Hanging Knee Raise",
+			"Hanging Leg Raise",
+			"Plank",
+			"Side Plank"
+		]
+	},
+	{
+		category: "Cardio",
+		exercises: [
+			"Cycling",
+			"Elliptical Trainer",
+			"Rowing Machine",
+			"Running (Outdoor)",
+			"Running (Treadmill)",
+			"Stationary Bike",
+			"Swimming",
+			"Walking"
+		]
+	}
+];
