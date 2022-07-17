1aa473a2a8a02c7e0c04e3fb6324e2acf90a3020
Przemek Gałęzki <przemekgg2002@gmail.com>
2022-07-17 16:54:19 +0200

    add: function inside use effect to check if user is logged

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
-import {useContext} from 'react'
+import {useContext, useEffect} from 'react'
 import {observer} from 'mobx-react-lite'
-import MainPage from './page-components/mainpage.js'
-import Dashboard from './page-components/common/dashboard/dashboard.js'
-import PageStore from './page-components/states-store/states/page-store.js'
+import MainPage from './page-components/mainpage'
+import Dashboard from './page-components/common/dashboard/dashboard'
+import PageStore from './page-components/states-store/states/page-store'
+import UserStore from './page-components/states-store/states/user-store'
+import { checkIfUserLogged } from './page-components/auth'
 
 const App = observer(() => {
 	const pageStore = useContext(PageStore)
+		const userStore = useContext(UserStore)
+
+	useEffect(() => {
+		checkIfUserLogged(userStore)
+	})
+	
 	return (
 		<>{pageStore.dashboardVisible === false
 			? <MainPage />
