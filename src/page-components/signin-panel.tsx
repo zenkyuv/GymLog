85a5896ad453f12c830352051997302cec943ece
Przemek Gałęzki <przemekgg2002@gmail.com>
2022-07-20 22:25:56 +0200

    add: data test ids

--- src/page-components/signin-panel.tsx
+++ src/page-components/signin-panel.tsx
@@ -53,17 +53,17 @@ export const SignIn = ({ setClick }: SignProps) => {
 							<LockOutlinedIcon />
 						</Avatar>
 						<Typography component="h1" variant="h5">Sign in</Typography>
-						<Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+						<Box data-testid="form" component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
 							<TextField margin="normal" required fullWidth id="email" label="Email Address"
-								name="email" autoComplete="email" autoFocus />
+								name="email" autoComplete="email" autoFocus inputProps={{ "data-testid": "email" }} />
 							<TextField margin="normal" required fullWidth name="password" label="Password"
-								type="password" id="password" autoComplete="current-password" />
+								type="password" id="password" autoComplete="current-password" inputProps={{ "data-testid": "password" }} />
 							<FormControlLabel control={<Checkbox value="remember" color="primary" />}
 								label="Remember me"/>
-							<Button type="submit" fullWidth variant="contained" sx={{ mt: 3, mb: 2 }}>
+							<Button  type="submit" data-testid="siginin-button" fullWidth variant="contained" sx={{ mt: 3, mb: 2 }}>
 								Sign In
 							</Button>
-							<Button type="submit" fullWidth variant="contained" color='secondary'
+							<Button type="button" fullWidth variant="contained" color='secondary'
 								onClick={() => setClick(false)} sx={{ mt: 1, mb: 2 }}>
 								Cancel
 							</Button>
