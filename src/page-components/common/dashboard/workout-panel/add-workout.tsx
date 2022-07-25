ebbf10ea590b3af285e9b8edd20f174a35d13ec3
Przemek Gałęzki <przemekgg2002@gmail.com>
2022-07-25 21:47:28 +0200

    add: print workout, open control panel on data grid row click

--- src/page-components/common/dashboard/workout-panel/add-workout.tsx
+++ src/page-components/common/dashboard/workout-panel/add-workout.tsx
@@ -1,16 +1,21 @@
 import {
 	DataGrid,
 	GridColDef,
+	GridColumns,
 	GridRowId,
+	GridRowsProp,
+	GridToolbar,
+	GridToolbarContainer,
+	GridToolbarExport,
 } from '@mui/x-data-grid'
 import { observer } from 'mobx-react'
 import ohp from "../../../../images/ohp.jpg"
 import ControlPanel from './ex-control-panel'
 import renderExercises from './renderExercises'
 import { useContext, useEffect, useState } from 'react'
+import { renderButtons } from '../calendar/calendar-buttons'
 import { WorkoutOptions } from '../../../../types/interfaces'
 import ClickAwayListener from '@mui/material/ClickAwayListener'
-import { renderButtons } from '../calendar/calendar-buttons'
 import UserStore from '../../../states-store/states/user-store'
 import loadingIndicator from '../../../../images/loading-indicator-2.svg'
 import styles from '../../../../component-styles/workout-panel.module.css'
@@ -29,6 +34,11 @@ const WorkoutPanel = observer((
 		exercise: undefined,
 		controlPanel: false,
 	})
+	const [controlPanel, setControlPanel] = useState({
+		category: undefined,
+		exercise: undefined,
+		showPanel: false
+	})
 	const [categories, setCategories]:any = useState()
 	const daysOfCurrentMonths = new Date(year, month, 0).getDate()
 	const [selectionModel, setSelectionModel] = useState<GridRowId[]>([])
@@ -38,9 +48,23 @@ const WorkoutPanel = observer((
 		exercises: undefined
 	})
 
-	useEffect( () => {
+	useEffect(() => {
 	renderCategoriesAndExercises(userStore).then(e => setCategories(e))
 	}, [])
+
+	useEffect(() => {
+		setControlPanel({category: undefined, exercise: undefined, showPanel: false})
+	}, [yearAndMonth])
+
+	function CustomToolbar() {
+  return (
+    <GridToolbarContainer>
+			<GridToolbarExport
+				printOptions={{allColumns: true}}
+			/>
+    </GridToolbarContainer>
+  );
+}
 
 	const renderCategories = () => {
 		return (
@@ -80,24 +104,27 @@ const WorkoutPanel = observer((
 	const [databaseYear, databaseMonth, databaseDay] = userStore.databaseTime
 		? userStore.databaseTime
 		: [0, 0, 0]
-	const databaseTimeEqualsFrontend = databaseYear === year && databaseMonth === month && databaseDay === day
+	const dataExists = databaseYear === year && databaseMonth === month && databaseDay === day
 	const weight = data?.map(data => data.weight) ? data?.map(data => data.weight).flat() : undefined
 	const reps = data?.map(data => data.reps) ? data?.map(data => data.reps).flat() : undefined
-	const index = weight
-	const rows = index !== undefined
-			? data
+	const rows: GridRowsProp = data !== undefined
+			? weight.map((_e, i) => ({
+					reps: reps[i],
+					weight: weight[i],
+					id: i + 1,
+						}))
 		: []
 
-	const columns: GridColDef[] = [
+	const columns: GridColumns = [
 		{ field: 'id', headerName: 'Set', width: 180 },
-		{ field: 'weight', headerName: 'Weight', width: 180 },
-		{ field: 'reps', headerName: 'Reps', width: 180 }
+		{ field: 'weight', headerName: 'weight', width: 180 },
+		{ field: 'reps', headerName: 'reps', width: 180 }
 	]
 
 	return (
 		<div className={styles["workout-cnt"]}>
 
-			{databaseTimeEqualsFrontend && !categoriesPanel.controlPanel
+			{dataExists && !categoriesPanel.controlPanel
 				? (<div className={styles["time-cnt-container"]}>
 					{changeTimeBtns}
 					{addExerciseBtn('add-button-top')}
@@ -106,38 +133,46 @@ const WorkoutPanel = observer((
 
 			{userStore.dbDataLoading && !categoriesPanel.controlPanel
 				? (<div className={styles.center}>
-					<img alt="loading" className={styles["loading-indicator"]} src={loadingIndicator}/>
-					</div>)
+					<img alt="loading" className={styles["loading-indicator"]} src={loadingIndicator} />
+				</div>)
 				: categoriesPanel.showCategories
 					? (renderCategories())
-					: databaseTimeEqualsFrontend && !categoriesPanel.controlPanel
-						? (<div className={styles["exercise-cnt"]}>
-							{rows.map(({ weight, reps, exercise }) => (
-								<>
-									<h3>{exercise}</h3>
-									<ClickAwayListener onClickAway={() => setSelectionModel([])}
-									style={{ height: '100%', width: '100%' }}>
-									<DataGrid className={styles["grid-height"]} selectionModel={selectionModel}
-										onSelectionModelChange={(selectionModel) => setSelectionModel(selectionModel)}
-										rows={weight.map((_e, i) => ({
-											reps: reps[i],
-											weight: weight[i],
-											id: i + 1,
-										}))}
-										pageSize={5}
-										columns={columns} />
-									</ClickAwayListener>
-								</>
-							))}
-							</div>)
+					: dataExists && !categoriesPanel.controlPanel
+						? controlPanel.showPanel
+							? <ControlPanel
+								exercise={controlPanel.exercise}
+								category={controlPanel.category}
+								yearAndMonth={yearAndMonth}
+								userStore={userStore} />
+							: (<div className={styles["exercise-cnt"]}>
+								{data?.map(({ weight, reps, exercise, category }) => (
+									<div style={{ minHeight: "100%", position: "relative" }}>
+										<h3>{exercise}</h3>
+										<ClickAwayListener onClickAway={() => setSelectionModel([])}
+										style={{ height: '100%', width: '100%' }}>
+											<DataGrid className={styles["grid-height"]} selectionModel={selectionModel}
+											onSelectionModelChange={(selectionModel) => setSelectionModel(selectionModel)}
+											rows={rows} pageSize={5} columns={columns} components={{ Toolbar: CustomToolbar }}
+											onRowClick={() => setControlPanel({
+												category: category,
+												exercise: exercise,
+												showPanel: true
+											})}
+											sx={{
+												'.MuiDataGrid-row': { cursor: "pointer" },
+											}} />
+										</ClickAwayListener>
+									</div>
+								))}
+								</div>)
 						: !categoriesPanel.controlPanel
-							? (<div className={styles.center}>{addExerciseBtn('add-button')}</div>)
-							: (<ControlPanel databaseTimeEqualsFrontend={databaseTimeEqualsFrontend}
-									exercise={categoriesPanel.exercise}
-									category={categoriesPanel.category}
-									yearAndMonth={yearAndMonth}
-									userStore={userStore} />)}
-
+							? <div className={styles.center}>{addExerciseBtn('add-button')}</div>
+							: (<ControlPanel
+								exercise={categoriesPanel.exercise}
+								category={categoriesPanel.category}
+								yearAndMonth={yearAndMonth}
+								userStore={userStore} />)
+							} 
 		</div>
 	)
 	}
