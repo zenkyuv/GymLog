72e8b9f7691534f0820540bf449ec0234c80ecac
Przemek Gałęzki <przemekgg2002@gmail.com>
2022-07-29 13:59:25 +0200

    add: buttons to open workout chart and exercise history

--- src/page-components/common/dashboard/workout-panel/control-panel.tsx
+++ src/page-components/common/dashboard/workout-panel/control-panel.tsx
@@ -11,14 +11,18 @@ import { ClickAwayListener } from '@mui/material'
 import { ControlPanelData } from '../../../../types/interfaces'
 import { getData, removeDocument, setDocument } from '../../../firestore-database'
 import styles from '../../../../component-styles/control-panel.module.css'
+import ExerciseHistory from './exercise-history'
+import WorkoutChart from './workout-chart'
 
 const ControlPanel = (
 	{userStore,
 	exercise,
 	category,
-	yearAndMonth}: ControlPanelData) => {
+	yearAndMonth}:ControlPanelData) => {
 	
 	const [show, setShowEl] = useState(false)
+	const [historyPanel, setHistoryPanel] = useState(false)
+	const [workoutChart, setWorkoutChart] = useState(false)
 	const [selectionModel, setSelectionModel] = useState<GridRowId[]>([])
 	interface State {
 		weight: number | string
@@ -83,44 +87,46 @@ const ControlPanel = (
 
 	return (
 		<div className={`${styles.row} ${styles["flex-wrap"]}`}>
-			<form className={`${styles.column} ${styles["add-panel"]}`} onReset={handleReset} onSubmit={handleSubmit}>
-				<div className={styles["text-container"]}>
-					<h4>History</h4>
-					<h4>Graph</h4>
-				</div>
-				<h1>{exercise}</h1>
-				WEIGHT(kgs):
-				<TextField type="number" id={styles["outlined-start-adornment"]} placeholder="kg"
-					value={values.weight} onChange={handleChange('weight')}/>
-				REPS:
-				<TextField type="number" value={values.reps} onChange={handleChange('reps')}
-					id={styles["outlined-start-adornment"]} placeholder="reps"/>
-				<div className={styles["flex-gap"]}>
-					<Button type="submit"
-						variant="contained" color="success">
-						{isSelected
-							? <span>Update</span>
-							: <span>Save</span>}
-					</Button>
-					<Button type="reset" color={isSelected ? 'error' : 'primary'}
-						onClick={() => getData(userStore, yearAndMonth)} variant="contained">
-						{isSelected
-							? (<span onClick={() => removeDocument(selectionModel, userStore, exercise, yearAndMonth, category)}>
-								Delete </span>)
-							: (<span>Clear</span>)}
-					</Button>
-				</div>
-			</form>
-			
+			{historyPanel
+				? <ExerciseHistory category={category} exercise={exercise} />
+				: workoutChart
+					? <WorkoutChart /> 
+					: <><form className={`${styles.column} ${styles["add-panel"]}`} onReset={handleReset} onSubmit={handleSubmit}>
+					<div className={styles["text-container"]}>
+						<h4 onClick={() => setHistoryPanel(true)}>History</h4>
+						<h4 onClick={() => setWorkoutChart(true)}>Graph</h4>
+					</div>
+					<h1>{exercise}</h1>
+					WEIGHT(kgs):
+					<TextField type="number" id={styles["outlined-start-adornment"]} placeholder="kg"
+						value={values.weight} onChange={handleChange('weight')} />
+					REPS:
+					<TextField type="number" value={values.reps} onChange={handleChange('reps')}
+						id={styles["outlined-start-adornment"]} placeholder="reps" />
+					<div className={styles["flex-gap"]}>
+						<Button type="submit"
+							variant="contained" color="success">
+							{isSelected
+								? <span>Update</span>
+								: <span>Save</span>}
+						</Button>
+						<Button type="reset" color={isSelected ? 'error' : 'primary'}
+							onClick={() => getData(userStore, yearAndMonth)} variant="contained">
+							{isSelected
+								? (<span onClick={() => removeDocument(selectionModel, userStore, exercise, yearAndMonth, category)}>
+									Delete </span>)
+								: (<span>Clear</span>)}
+						</Button>
+					</div>
+				</form>
 				<div className={`${styles.row} ${styles["info-panel"]}`}>
-					<ClickAwayListener onClickAway={() => setSelectionModel([])}
-						style={{ height: '100%', width: '100%' }}>
-						<DataGrid rows={databaseData.rows} pageSize={5} columns={columns}
-							selectionModel={selectionModel} onSelectionModelChange={
-								(selectionModel) => setSelectionModel(selectionModel)
-							}/>
-					</ClickAwayListener>
-				</div>
+						<ClickAwayListener onClickAway={() => setSelectionModel([])}
+							style={{ height: '100%', width: '100%' }}>
+							<DataGrid rows={databaseData.rows} pageSize={5} columns={columns}
+								selectionModel={selectionModel} onSelectionModelChange={(selectionModel) => setSelectionModel(selectionModel)} />
+						</ClickAwayListener>
+					</div></>
+			}
 			
 		</div>
 	)
