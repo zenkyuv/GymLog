066aab5af07a606c9ba4f4679682120ae138e9ee
Przemek Gałęzki <przemekgg2002@gmail.com>
2022-08-12 16:40:35 +0200

    add: workout chart component, workout volume stats

---
+++ src/page-components/common/dashboard/workout-panel/workout-chart.tsx
@@ -0,0 +1,44 @@
+import { useContext, useEffect, useState } from 'react';
+import { LineChart, Line, XAxis, CartesianGrid, YAxis, Tooltip } from 'recharts';
+import { getExercisesHistory } from '../../../firestore-database';
+import UserStore from '../../../states-store/states/user-store';
+
+const WorkoutChart = (workout) => {
+	const userStore = useContext(UserStore)
+	const [exerciseHistory, setExerciseHistory] = useState(undefined)
+	useEffect(() => {
+		getExercisesHistory(userStore, workout.category, workout.exercise)
+			.then(data => Promise.all(data).then(data =>
+				setExerciseHistory(data.filter(data => data !== undefined))))
+			.catch(err => console.log(err))
+	}, [])
+	const filteredExerciseHistory = exerciseHistory?.map(({ workoutData, timeData }) =>
+		Object.keys(workoutData).
+		filter(key => key == workout.exercise).
+			reduce((obj, key) => { return Object.assign(obj, { [key]: workoutData[key], timeData }) }, {}))
+		.filter(value => Object.keys(value).length !== 0)
+	const data = filteredExerciseHistory?.map((history, i) =>
+	({timeData: history?.timeData, volume: history?.[workout.exercise]?.reps
+		.map((rep, i) => rep * history?.[workout.exercise]?.weight[i]).reduce((acc,val) => acc + val, 0) }))
+	const workoutVolume = filteredExerciseHistory?.map((history, i) => history?.[workout.exercise]?.reps
+		.map((rep, i) => rep * history?.[workout.exercise]?.weight[i]))
+
+	const renderLineChart = (
+	<LineChart width={400} height={400} data={data}>
+		<Line type="monotone" dataKey="volume" stroke="#8884d8" />
+		<CartesianGrid stroke="#ccc" />
+		<XAxis dataKey="timeData" />
+		<YAxis />
+		<Tooltip />
+	</LineChart>
+	)
+
+	return (
+		<div>
+			<h1>Chart</h1>
+			{renderLineChart}
+		</div>
+	)
+}
+
+export default WorkoutChart
