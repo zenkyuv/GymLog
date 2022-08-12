b35f8762ac0a280513d3d74d91ce0b1e66be8801
Przemek Gałęzki <przemekgg2002@gmail.com>
2022-08-12 22:15:53 +0200

    add: network indicator to notify if no connection

--- src/page-components/common/dashboard/calendar/calendar.tsx
+++ src/page-components/common/dashboard/calendar/calendar.tsx
@@ -12,8 +12,9 @@ import {
 	handleMonthNavForwardButtonClick,
 } from './calendar-buttons'
 import classNames from 'classnames'
-import { getCategories } from '../../../firestore-database'
 import UserStore from '../../../states-store/states/user-store'
+import noConnectionImg from "../../../../images/cloud-offline.svg"
+import { getCategories, getData } from '../../../firestore-database'
 import { ChangeEvent, useContext, useEffect, useState } from 'react'
 import styles from '../../../../component-styles/calendar.module.css'
 
@@ -34,6 +35,7 @@ export const Calendar = ({
 	let [year, month] = yearAndMonth
 	const userStore = useContext(UserStore)
 	const [categories, setCategory] = useState<string[][]>()
+	const [isOnline, setStatus] = useState(navigator.onLine)
 	const [yearNow, monthNow, dayNow]: number[] = today
 
 	let currentMonthDays = createDaysForCurrentMonth(year, month)
@@ -65,6 +67,11 @@ export const Calendar = ({
 	const component = 'calendar'
 
 	useEffect(() => {
+		window.addEventListener('online', () => setStatus(true))
+		window.addEventListener('offline', () => setStatus(false))
+	}, [])
+
+	useEffect(() => {
 		function getWorkoutIndicators(day: { dateString: any; dayOfMonth?: number; isCurrentMonth?: boolean }) {
 		const yearAndMonth = day.dateString.split("-").map((element: any) => {
 			return Number(element)
@@ -75,33 +82,36 @@ export const Calendar = ({
 		Promise.all(b).then((arr:any) => {
 				setCategory(arr)
 		})
-	}, [yearAndMonth])
+	}, [yearAndMonth, isOnline])
 
 	return (
 		<div className={styles["calendar-root"]}>
-			<div className={styles["navigation-header"]}>
+			{isOnline
+				? <><div className={styles["navigation-header"]}>
 				<div className={styles["month-nav-arrow-buttons"]}>
 					<button
 						data-testid="button-back"
-						onClick={() =>
-							handleMonthNavBackButtonClick(
-								{component,
+						onClick={() => handleMonthNavBackButtonClick(
+							{
+								component,
 								yearAndMonth,
 								onYearAndMonthChange,
-								userStore}
-							)}>
+								userStore
+							}
+						)}>
 						{' '}
 						prev{' '}
 					</button>
 					<button
 						data-testid="button-forward"
-						onClick={() =>
-							handleMonthNavForwardButtonClick(
-								{component,
+						onClick={() => handleMonthNavForwardButtonClick(
+							{
+								component,
 								yearAndMonth,
 								onYearAndMonthChange,
-								userStore}
-							)}>
+								userStore
+							}
+						)}>
 						next
 					</button>
 				</div>
@@ -117,34 +127,39 @@ export const Calendar = ({
 							{label}
 						</option>))}
 				</select>
-			</div>
-			<div className={styles["days-of-week"]}>
-				{daysOfWeek.map((day, index) => (
-					<div key={day} className={classNames(styles['day-of-week-header-cell'],
-						{[styles['weekend-day']]: [6, 0].includes(index),})}>
-						{day}
-					</div>))}
-			</div>
-			<div className={styles["days-grid"]}>
-				{calendarGridDayObjects?.map((day, i) => (
-					<div key={day.dateString} className={classNames(styles['day-grid-item-container'],
-						{[styles['weekend-day']]: isWeekendDay(day.dateString),
-						 [styles['current-month']]: day.isCurrentMonth,
-						 [styles['not-current-month']]: !day.isCurrentMonth})}>
-						<div className={styles["day-content-wrapper"]}>
-							{day.dayOfMonth === dayNow &&
-							year === yearNow &&
-							month === monthNow &&
-							day.isCurrentMonth
-							? (<span className={styles.today}>{renderDay(day)}</span>)
-							: (renderDay(day))}
-							<div className={styles["indicators-wrapper"]}>
-								{categories?.[i]?.map((c: string, i: number) =>
-									<div key={i} className={styles[`${c}-indicator`]}></div>)}
+			</div><div className={styles["days-of-week"]}>
+					{daysOfWeek.map((day, index) => (
+						<div key={day} className={classNames(styles['day-of-week-header-cell'],
+							{ [styles['weekend-day']]: [6, 0].includes(index), })}>
+							{day}
+						</div>))}
+				</div><div className={styles["days-grid"]}>
+					{calendarGridDayObjects?.map((day, i) => (
+						<div key={day.dateString} className={classNames(styles['day-grid-item-container'],
+							{
+								[styles['weekend-day']]: isWeekendDay(day.dateString),
+								[styles['current-month']]: day.isCurrentMonth,
+								[styles['not-current-month']]: !day.isCurrentMonth
+							})}>
+							<div className={styles["day-content-wrapper"]}>
+								{day.dayOfMonth === dayNow &&
+									year === yearNow &&
+									month === monthNow &&
+									day.isCurrentMonth
+									? (<span className={styles.today}>{renderDay(day)}</span>)
+									: (renderDay(day))}
+								<div className={styles["indicators-wrapper"]}>
+									{categories?.[i]?.map((c: string, i: number) => <div key={i} className={styles[`${c}-indicator`]}></div>)}
+								</div>
 							</div>
-						</div>
-					</div>))}
-			</div>
+						</div>))}
+					</div></>
+				: <div className={styles["noconnection"]}>
+					<div>
+						<img src={noConnectionImg} alt="noconnection img" /><span>No internet connection</span>
+					</div>
+					<span>Go online to get latest workout data.</span>
+				</div>}
 		</div>
 	)
 }
