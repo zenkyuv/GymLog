4b4b4f7d1af89324e246f21a25ab1664fa8997f8
Przemek Gałęzki <przemekgg2002@gmail.com>
2022-08-12 21:09:49 +0200

    style: header is fixed if not inView

--- src/page-components/common/header.tsx
+++ src/page-components/common/header.tsx
@@ -1,34 +1,27 @@
 import { logout } from '../auth'
-import classNames from 'classnames'
-import SignIn from '../signin-panel'
 import { observer } from 'mobx-react-lite'
 import { useContext, useEffect, useState } from 'react'
 import UserStore from '../states-store/states/user-store'
 import PageStore from '../states-store/states/page-store'
 import styles from '../../component-styles/header.module.css'
+import { useInView } from 'react-intersection-observer';
 
 const Header = observer(() => {
 	
 	const userStore = useContext(UserStore)
 	const pageStore = useContext(PageStore)
-	const [clicked, setClick] = useState(false)
-
-	useEffect(() => {
-		if (userStore.userIsLogged) {
-			setClick(false)
-		} else if (userStore.userIsLogged && pageStore.dashboardVisible === false) {
-			setClick(true)
-		}
-	}, [pageStore.dashboardVisible, userStore.userIsLogged])
+	const { ref, inView, entry } = useInView({
+		/* Optional options */
+		threshold: 0,
+	})
 
 	return (
-		<header className={classNames(styles['header-area header-sticky'])}>
-			<nav className={styles["main-nav"]}>
-				<a href="index.html" className={styles.logo}>
-					Gym<span className={styles.text}>Log</span>
-					<span className={styles.line}>-</span>
-					<span className={styles.info}>simple workout tracker</span>
-				</a>
+		<header ref={ref} className={styles['header-area']}>
+			<div className={inView ? styles["container"] : `${styles['container']} ${styles.fixed}`}>
+				<nav className={styles["main-nav"]}>
+				<h3 className={styles.logo}>
+					GYM<span>LOG</span>
+				</h3>
 				<div className={styles["sign-buttons"]}>
 					<ul className={styles.nav}>
 
@@ -43,7 +36,7 @@ const Header = observer(() => {
 						
 						{userStore.userIsLogged === false
 							? (<li className={styles["main-button"]}>
-									<a href="#/" data-testid="signin-main" onClick={() => setClick(true)}>
+									<a href="#/" data-testid="signin-main" onClick={() => pageStore.openLoginForms()}>
 										Sign in
 									</a>
 							</li>)
@@ -56,8 +49,8 @@ const Header = observer(() => {
 					</ul>
 					<span className={styles.bars}>---</span>
 				</div>
-			</nav>
-			{clicked && userStore.userIsLogged === false ? <SignIn setClick={setClick} /> : null}
+				</nav>
+				</div>
 		</header>
 	)
 })
